a9cf9f687a06c2d36614c97e09ce90c56bbca511
Ezequiel Giannoni <69121634+ezequielgiannoni@users.noreply.github.com>
2021-08-30 12:35:30 -0300

    2do Commit
    Paso 1-B

--- ProyectoWeb(Estructura)/main.js
+++ ProyectoWeb(Estructura)/main.js
@@ -5,12 +5,31 @@ var numero = 3495;
 var concatenacion = nombre + " " + direccion + " " + numero;
 
 function MuestraNombre(nombre, direccion, numero){
-    var datos = document.getElementById("datos");
-    datos.innerHTML= `
+    var misDatos = `
         <h1>Soy un salon de eventos</h1>
         <h2>Mi nombre es: ${nombre}</h2>
         <h3>Mi direccion es: ${direccion}</h3>
         <h4>Mi numero es: ${numero} </h4>`;
+
+    return misDatos;
 }
 
-MuestraNombre("MoonLight", "Rivadavia", 3495);
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML= MuestraNombre("AgaveAmica", "Lopez de Gomara", 3275);
+}
+
+imprimir();
+
+var nombres = ['Braiton', 'Ezequiel'];
+/*alert(nombres[0]);
+*/
+document.write('<h1>Listado de nombres</h1>');
+/*
+for(i = 0; i < nombres.length; i++){
+    document.write(nombres[i] + '<br/>');
+}
+*/
+nombres.forEach(function(nombre) {
+    document.write(nombre + '<br/>');
+});
